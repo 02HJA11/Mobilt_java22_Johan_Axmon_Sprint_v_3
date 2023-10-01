b15c20a3e7f9f99f628085c4eeca4626b34005f5
Johan <hansjohanaxmon@gmail.com>
2023-10-01 19:22:30 +0200

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/services.dart';
+import 'package:translate/pages/home_page.dart';
+import 'package:translate/pages/translate_page.dart';
+import 'package:translate/pages/history_page.dart';
+import 'firebase_options.dart';
+
+
+void main() {
+  runApp(const App());
+
+}
+
+class App extends StatelessWidget {
+  const App({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      future: Firebase.initializeApp(
+          options: DefaultFirebaseOptions.currentPlatform
+      ),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          return const MyApp();
+        }
+        if (snapshot.hasError) {
+          return MaterialApp(
+            home: Scaffold(
+              body: Center(
+                child: Text('Error initializing Firebase: ${snapshot.error}'),
+              ),
+            ),
+          );
+        }
+        return const MaterialApp(
+          home: Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Translate App',
+      theme: ThemeData(primarySwatch: Colors.indigo),
+      initialRoute: '/',
+      routes: {
+        '/': (context) => const HomePage(),
+        '/translate': (context) => const TranslatePage(),
+        '/history': (context) => const HistoryPage(),
+      },
+    );
+  }
+}
---
+++ lib/pages/history_page.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+
+class HistoryPage extends StatefulWidget {
+  const HistoryPage({Key? key}) : super(key: key);
+
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  final CollectionReference collectionReference = FirebaseFirestore.instance.collection('translations');
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Translation History')),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: collectionReference.snapshots(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError) {
+            return Center(child: Text('Error: ${snapshot.error}'));
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return const Center(child: CircularProgressIndicator());
+          }
+
+          if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+            return const Center(child: Text('No translations found.'));
+          }
+
+          //none of these are used since scrolling seems to work most of the time
+          //single scroll view/ single child scroll view https://api.flutter.dev/flutter/widgets/SingleChildScrollView-class.html
+
+          // expanded / shrink box
+          List<QueryDocumentSnapshot> translations = snapshot.data!.docs;
+
+          return ListView.builder(
+            itemCount: translations.length,
+            itemBuilder: (context, index) {
+              return ListTile(
+                title: Text(translations[index]['originalText'] ?? 'No original text'),
+                subtitle: Text(translations[index]['translatedText'] ?? 'No translation'),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
